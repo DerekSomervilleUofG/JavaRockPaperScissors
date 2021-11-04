32a4a631b4d43fcddb964d9bb4f006aa679d9675
DerekSomervilleUofG <del.somerville@gmail.com>
2021-11-04 15:16:51 +0000

    Create a main

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[ ] args){
+        JavaRockPaperScissor rockPaperScissor = new JavaRockPaperScissor();
+        rockPaperScissor.run();
+    }
+}
